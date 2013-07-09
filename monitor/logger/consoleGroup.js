c25d3bdb962f13e3ac22865e22925ca26cff593f
Brian Cavalier <brian@hovercraftstudios.com>
2013-07-09 10:35:48 -0400

    See #167. Use console.log + JSON.stringify in IE9

--- monitor/logger/consoleGroup.js
+++ monitor/logger/consoleGroup.js
@@ -15,57 +15,51 @@ define(function(require) {
 
 	array = require('../array');
 
-	if(typeof console === 'undefined') {
-		// No console, give up, but at least don't break
-		log = consoleNotAvailable;
-	} else {
-		if (console.warn && console.dir) {
-			// Sensible console found, use it
+	if(typeof console !== 'undefined') {
+		// console exists, test it's capabilities.  We still might
+		// end up without suitable console capabilities
+		if (console.groupCollapsed && console.warn) {
+			// Modern console found, use it
 			warn = function (x) {
 				console.warn(x);
 			};
-		} else {
-			// IE8 has console.log and JSON, so we can make a
-			// reasonably useful warn() from those.
-			// Credit to webpro (https://github.com/webpro) for this idea
-			if (console.log && typeof JSON != 'undefined') {
-				warn = function (x) {
-					console.log(typeof x === 'string' ? x : JSON.stringify(x));
-				};
-			}
-		}
 
-		if(!warn) {
-			// Couldn't find a suitable console logging function
-			// Give up and just be silent
-			log = consoleNotAvailable;
-		} else {
-			if(console.groupCollapsed) {
-				warnAll = function(msg, list) {
-					console.groupCollapsed(msg);
-					try {
-						array.forEach(list, warn);
-					} finally {
-						console.groupEnd();
-					}
-				};
-			} else {
-				warnAll = function(msg, list) {
-					warn(msg);
-					warn(list);
-				};
-			}
-
-			log = function(rejections) {
-				if(rejections.length) {
-					warnAll('[promises] Unhandled rejections: '
-						+ rejections.length, rejections);
-				} else {
-					warn('[promises] All previously unhandled rejections have now been handled');
+			warnAll = function(msg, list) {
+				console.groupCollapsed(msg);
+				try {
+					array.forEach(list, warn);
+				} finally {
+					console.groupEnd();
 				}
 			};
-		}
+		} else if (console.log && typeof JSON != 'undefined') {
+			// IE8 & 9 have console.log and JSON, so we can make a
+			// reasonably useful warn() from those.
+			// Credit to webpro (https://github.com/webpro) for this idea
+			warn = function (x) {
+				console.log(typeof x === 'string' ? x : JSON.stringify(x));
+			};
 
+			warnAll = function(msg, list) {
+				warn(msg);
+				warn(list);
+			};
+		}
+	}
+
+	if(!warn) {
+		// Couldn't find a suitable console logging function
+		// Give up and just be silent
+		log = consoleNotAvailable;
+	} else {
+		log = function(rejections) {
+			if(rejections.length) {
+				warnAll('[promises] Unhandled rejections: '
+					+ rejections.length, rejections);
+			} else {
+				warn('[promises] All previously unhandled rejections have now been handled');
+			}
+		};
 	}
 
 	return log;
